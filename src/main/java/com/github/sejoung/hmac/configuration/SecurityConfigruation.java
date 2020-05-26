67ddd72f695561b1bcaafc760a26da8f944fdd0a
김세중 <sjkim2@iparking.co.kr>
2020-05-26 15:22:03 +0900

    Update SecurityConfigruation.java

--- src/main/java/com/github/sejoung/hmac/configuration/SecurityConfigruation.java
+++ src/main/java/com/github/sejoung/hmac/configuration/SecurityConfigruation.java
@@ -3,7 +3,6 @@ package com.github.sejoung.hmac.configuration;
 import com.github.sejoung.hmac.filter.HmacFilter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -30,8 +29,7 @@ public class SecurityConfigruation extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        http.authorizeRequests().antMatchers(
-            HttpMethod.GET, "**").hasAnyRole("USER").and().formLogin()
+        http.authorizeRequests().antMatchers("**").hasAnyRole("USER").and().formLogin()
             .disable().csrf().disable()
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
             .exceptionHandling()
